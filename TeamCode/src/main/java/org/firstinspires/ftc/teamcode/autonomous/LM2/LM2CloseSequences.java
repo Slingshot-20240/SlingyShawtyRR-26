98a7a989a1ff9b61d4afd9b4fa0b949653c10867
Ishaan Desai <IshaanDesai21@gmail.com>
2025-11-05 12:31:06 -0600

    have params sequence change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LM2/LM2CloseSequences.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LM2/LM2CloseSequences.java
@@ -27,46 +27,15 @@ public class LM2CloseSequences {
     }
 
 
-    public Action preparePreloads() {
-        return new ParallelAction(
-                intake.in()
-
-        );
-    }
-    public Action scorePreloads() {
+    public Action scoreSet(double speedUpTime, double transferTime) {
         return new SequentialAction(
 
                 intake.in(),
                 new SequentialAction(
-                        //TODO - Tune the time the flywheel tune to get to good speed
-                        new SleepAction(3.3),
-                        hws.transferUpFor(4)
-
-
-                )
-
-
-        );
-    }
-
-
-    /**
-     * Code todo
-     * keep in mind to wait between each shot
-     * how do we figure out if a ball has been shot or not with CR servo
-     * Transfer only turns on once shooter power has been set
-     * Intake power runs constantly
-     * Hood is angled to shoot position
-     */
-    public Action scoreSet() {
-        return new ParallelAction(
-
-                intake.in(),
-                new SequentialAction(
-                        //TODO - Tune the time the flywheel tune to get to good speed
-                        new SleepAction(0.5),
-                        hws.transferUpFor(4),
-                        shooter.idle()
+                        //TODO - Flywheel is already near speed, tune the time it takes to adjust. should be very low
+                        new SleepAction(speedUpTime),
+                        //TODO - Tune the transfer time to shoot 3 balls
+                        hws.transferUpFor(transferTime),
 
                 )
 
@@ -74,34 +43,18 @@ public class LM2CloseSequences {
     }
 
 
-    //TODO - for both intakeSet AND prepare for Set should we hardcode values here or keep parameters
-    // because then they would both be the same function, intaking while transferring
     /**
-     * Intakes while transfer
+     * Intakes while hotdogging
      */
-    public Action intakeSet() {
-        return new SequentialAction(
-                new ParallelAction(
-                        transfer.hotdog(),
-                        intake.in()
-                //TODO - Tune this transfer value to determine how high up the first ball goes
-                       // transfer.hotdog()
-                ),
-                shooter.out()
-
-        );
-    }
-
-    /**
-     * Intake in while transfer
-     */
-    public Action prepareForSet() {
+    public Action intakeSet(int shooterPower) {
         return new ParallelAction(
                 intake.in(),
                 transfer.hotdog(),
-                shooter.out()
+                new ShooterAction(shooter.outtake1, shooter.outtake2, shooterPower)
+
         );
     }
+
 
 }
 
