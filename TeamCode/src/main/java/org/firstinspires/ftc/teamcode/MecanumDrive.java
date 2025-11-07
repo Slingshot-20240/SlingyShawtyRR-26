10d27dc0f385087ad87e16afd9d5e9b8e54b1739
ishaandesai <IshaanDesai21@gmail.com>
2025-11-07 01:17:41 -0600

    Ishaan Tuning Pinpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -78,9 +78,9 @@ public final class MecanumDrive {
 
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 70;
-        public double minProfileAccel = -25;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 75;
+        public double minProfileAccel = -30;
+        public double maxProfileAccel = 60;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
@@ -88,10 +88,10 @@ public final class MecanumDrive {
 
         // path controller gains
         public double axialGain = 11;
-        public double lateralGain = 5;
+        public double lateralGain = 6;
         public double headingGain = 4; // shared with turn
 
-        public double axialVelGain = 0.0;
+        public double axialVelGain = 0.5;
         public double lateralVelGain = 0.0;
         public double headingVelGain = 0.0; // shared with turn
     }
