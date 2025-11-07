5ca28f469cd17641167f0d982e9245bf9827b6b9
James <jamesetam@gmail.com>
2025-11-06 21:40:25 -0600

    recompliance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/PythonLimelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/PythonLimelight.java
@@ -1,4 +1,78 @@
 package org.firstinspires.ftc.teamcode.subsystems.vision;
 
+import com.qualcomm.hardware.limelightvision.LLResultTypes.FiducialResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.Objects;
+import java.util.stream.*;
+
+
 public class PythonLimelight {
+    /*
+    FORMAT:
+    [
+        most recent obelisk ID,
+        most recent nav ID,
+        relocalized x based on nav ID,
+        relocalized y,
+        relocalized z,
+        relocalized yaw,
+        relocalized pitch,
+        relocalized roll
+    ]
+    */
+    Limelight3A limelight;
+    boolean isBlue;
+
+    public PythonLimelight(HardwareMap hw, boolean isBlue) {
+        limelight = hw.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(2);
+        limelight.start();
+        this.isBlue = isBlue;
+    }
+    public ObeliskLocation getObelisk(){
+        return ObeliskLocation.fromInt((int) limelight.getLatestResult().getPythonOutput()[0]);
+    }
+
+    public Pose3D getPose() {
+        double[] llResults = limelight.getLatestResult().getPythonOutput();
+        return new Pose3D(new Position(DistanceUnit.INCH, //TODO: figure this out LOL
+                                       llResults[2],
+                                       llResults[3],
+                                       llResults[4],
+                                       System.nanoTime()),
+
+                          new YawPitchRollAngles(AngleUnit.DEGREES, //TODO: figure this out LOLOLLLL
+                                                 llResults[5],
+                                                 llResults[6],
+                                                 llResults[7],
+                                                 System.nanoTime()));
+    }
+    public enum ObeliskLocation //measured by the location of the green
+    {
+        LEFT("GPP", 21), CENTER("PGP", 22), RIGHT("PPG", 23);
+
+        public final String order;
+        public final int ATnumber;
+        ObeliskLocation(String o, int AT){
+            order = o;
+            ATnumber = AT;
+        }
+
+        public static ObeliskLocation fromInt(int i){
+            for(ObeliskLocation ol : ObeliskLocation.values()){
+                if (ol.ATnumber == i)
+                    return ol;
+            }
+            return null;
+        }
+    }
+
 }
